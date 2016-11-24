36c78218804b950694e45a98e2da1066cc9e2e94
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-24 22:54:15 +0000

    update server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,22 @@ const app = express()
 app.use(morgan('dev'))
 app.use('/api', apiRouter)
 
+if (process.env.NODE_ENV !== 'production') {
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackConfig = require('../config/webpack/config.js')
+  const webpack = require('webpack')
+  const compiler = webpack(webpackConfig)
+
+  app.use(webpackDevMiddleware(compiler, {
+    hot: true,
+    publicPath: '/bundles/',
+    stats: {
+      colors: true,
+    },
+    historyApiFallback: true,
+  }))
+}
+
 app.listen(PORT, '0.0.0.0', (err) => {
   if (err) { console.log(err) }
   console.info('==> 🌎  Listening on port %s.', PORT)
