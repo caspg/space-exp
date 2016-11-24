c429584ec2662cf6ec183cbfda113821546fb920
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-24 22:50:58 +0000

    add webpack config

---
+++ config/webpack/config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    app: [
+      path.resolve(__dirname, '../src/index.jsx'),
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, '../public/bundles'),
+    filename: '[name].bundle.js',
+    publicPath: '/bundles/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    root: path.resolve(__dirname, '../src'),
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      react: path.resolve(__dirname, '../node_modules', 'react'),
+    },
+  },
+}
