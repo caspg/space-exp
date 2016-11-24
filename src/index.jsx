114885884d55e31e458ef45b689021c2a97cb411
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-24 23:16:24 +0000

    add styled components

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Hello = () => (
+  <Title>
+    Hello, World!
+  </Title>
+)
 
 render(
-  <div>Hello, World!</div>,
+  <Hello />,
   document.getElementById('app'),
 )
