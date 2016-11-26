dc9a02ff188c945cf7e27a6577971bf108fa0ec1
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-26 20:38:06 +0000

    move styled components to separate files

--- src/routes/ApodDetails/components/ApodDetailsView/ApodDetailsView.jsx
+++ src/routes/ApodDetails/components/ApodDetailsView/ApodDetailsView.jsx
@@ -1,31 +1,11 @@
 import React, { PropTypes } from 'react'
-import styled from 'styled-components'
 
-import colors from 'styles/constants/colors'
 import ApodDetails from '../ApodDetails'
-
-const DetailsContainer = styled.div`
-  text-align: center;
-  margin: 0 -5px;
-  margin-top: 100px;
-  margin-bottom: 100px;
-`
-
-const Wrapper = styled.div`
-  display: inline-block;
-`
-
-const Image = styled.img`
-  max-width:100%;
-  max-height:100%;
-`
-
-const DetailsWrapper = styled.div`
-  background-color: ${colors.grey300};
-  padding-top: 80px;
-  padding-bottom: 50px;
-  margin-top: -10px;
-`
+import {
+  DetailsContainer,
+  Wrapper,
+  DetailsWrapper,
+} from './StyledComponents'
 
 const ApodDetailsView = props => (
   <DetailsContainer>
@@ -42,7 +22,7 @@ ApodDetailsView.propTypes = {
   apod: PropTypes.shape({
     url: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    copyright: PropTypes.string.isRequired,
+    copyright: PropTypes.string,
     explanation: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
   }),
---
+++ src/routes/ApodDetails/components/ApodDetailsView/StyledComponents.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+import colors from 'styles/constants/colors'
+
+export const DetailsContainer = styled.div`
+  text-align: center;
+  margin: 0 -5px;
+  margin-top: 100px;
+  margin-bottom: 100px;
+`
+
+export const Wrapper = styled.div`
+  display: inline-block;
+`
+
+export const Image = styled.img`
+  max-width:100%;
+  max-height:100%;
+`
+
+export const DetailsWrapper = styled.div`
+  background-color: ${colors.grey300};
+  padding-top: 80px;
+  padding-bottom: 50px;
+  margin-top: -10px;
+`
