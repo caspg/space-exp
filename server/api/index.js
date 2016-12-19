36f7aa6814c9c5e627fe6c4053399fc058b7b183
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-19 22:55:22 +0100

    fetch apod by slug from DB

--- server/api/index.js
+++ server/api/index.js
@@ -1,18 +1,12 @@
 const express = require('express')
-const fs = require('fs')
 const moment = require('moment')
 
 const Apod = require('../db/schema/apod')
 
 const apiRouter = express.Router()
 
-const fetchApods = (callback) => {
-  fs.readFile('server/data/apods.json', 'utf8', (err, data) => {
-    if (err) throw err
-
-    callback(JSON.parse(data))
-  })
-}
+const serverErrorHandler = res =>
+  res.status(500).json({ message: 'There was some server error.' })
 
 apiRouter.get('/apods', (req, res) => {
   const queryDate = req.query.date || moment().format('YYYY-MM-DD')
@@ -35,22 +29,22 @@ apiRouter.get('/apods', (req, res) => {
         apods,
       })
     })
-    .catch(() => {
-      res.status(500).json({ message: 'some server error' })
-    })
+    .catch(() => { serverErrorHandler(res) })
 })
 
 apiRouter.get('/apods/:slug', (req, res) => {
   const { slug } = req.params
-  fetchApods((json) => {
-    const apod = json.filter(i => i.slug === slug)[0]
 
-    if (apod) {
-      res.json(apod)
-    } else {
-      res.status(404).json({})
-    }
-  })
+  Apod
+    .findOne({ slug }, '-_id -__v')
+    .then((apod) => {
+      if (apod) {
+        res.json(apod)
+      } else {
+        res.status(404).json({ message: 'Apod not found.' })
+      }
+    })
+    .catch(() => { serverErrorHandler(res) })
 })
 
 module.exports = apiRouter
