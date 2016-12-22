c263a180f75106299d2429bb2ac8a93edfe7c984
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-22 22:31:06 +0100

    add favicons and space-exp image

--- server/utils/serverSideRender/html/render.js
+++ server/utils/serverSideRender/html/render.js
@@ -17,9 +17,12 @@ module.exports = ({ content, styles, bootData, meta = defaultMeta }) => `
 
     <meta name="og:image" content="${meta.image}">
 
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,900" rel="stylesheet">
