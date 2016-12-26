1a65d414245d8f9ff7cbad6591de20ed12041117
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-26 16:29:55 +0100

    add cors to api

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 
 const { findCollection, findBySlug } = require('./apods')
 
 const apiRouter = express.Router()
 
-apiRouter.get('/apods', (req, res) => {
+const corsOptions = {
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+apiRouter.get('/apods', cors(corsOptions), (req, res) => {
   findCollection(req, res)
 })
 
-apiRouter.get('/apods/:slug', (req, res) => {
+apiRouter.get('/apods/:slug', cors(corsOptions), (req, res) => {
   findBySlug(req, res)
 })
 
