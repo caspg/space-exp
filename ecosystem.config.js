10ff0302fd8ae6391d8f851bc15dd4e2c3286305
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-26 13:52:33 +0100

    create pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const user = 'caspg'
+
+module.exports = {
+  apps: [
+    {
+      name: 'space-exp',
+      script: './server/index.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user,
+      host: '144.217.161.18',
+      ref: 'origin/master',
+      repo: 'git@bitbucket.org/caspg/space-exp-mvp.git',
+      path: `/home/${user}/applications/space-exp`,
+      'post-deploy': 'yarn install && yarn run build && pm2 startOrRestart ecosystem.json --env production',
+    },
+  },
+}
