c5a0a70cda088a744bae30c1331dca8b2ddb9186
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-26 16:34:01 +0100

    upate cors config

--- server/api/index.js
+++ server/api/index.js
@@ -6,6 +6,7 @@ const { findCollection, findBySlug } = require('./apods')
 const apiRouter = express.Router()
 
 const corsOptions = {
+  origin: '*',
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
