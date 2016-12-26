61808061f5878077bb34cf093da85eec4b14234e
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-26 16:01:48 +0100

    update config.dev.js

--- config/webpack/config.dev.js
+++ config/webpack/config.dev.js
@@ -7,16 +7,19 @@ const jsxLoader = config.module.loaders.filter(loader =>
 
 jsxLoader.loaders.push('eslint-loader')
 
+config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      PORT: JSON.stringify(process.env.PORT),
+    },
+  }),
+)
+
 if (process.env.ANALYZE) {
   const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer') // eslint-disable-line
 
   config.plugins.push(
     new BundleAnalyzerPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        PORT: JSON.stringify(process.env.PORT),
-      },
-    }),
   )
 }
 
