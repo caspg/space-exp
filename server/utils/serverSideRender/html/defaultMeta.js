81ee3312078733763185465b24a61b7eca6fa767
Kacper Golinski <kacper.golinski@gmail.com>
2016-12-27 13:39:33 +0100

    update image metatag

--- server/utils/serverSideRender/html/defaultMeta.js
+++ server/utils/serverSideRender/html/defaultMeta.js
@@ -1,5 +1,5 @@
 module.exports = {
   title: 'Space Experience - APOD',
   description: 'Space Experience - Astronomy Picture of the Day - brings you each day a stunning picture of our universe. Pictures and explanations are provided by apod.nasa.gov.',
-  image: 'http://018454f4.ngrok.io/space-exp.jpg',
+  image: 'https://spaceexperience.club/space-exp.jpg',
 }
